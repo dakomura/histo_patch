08df941947ba899c621fc3ee4807adc05e19fc66
Daisuke Komura <kdais-prm@m.u-tokyo.ac.jp>
2022-08-02 22:54:27 +0900

    bug fix

--- histo_patch/utils.py
+++ histo_patch/utils.py
@@ -1,7 +1,8 @@
 import os   
 
+# get WSI type(.svs or .ndpi)
 def get_wsi_type(wsifile):
-    ex = os.path.splittext(wsifile)[1]
-    if not ex in ['svs', 'ndpi']:
-        raise Exception('invalid file format %r' % wsifile)
-    return ex
+    ex = os.path.splitext(wsifile)[1]
+    if not ex in ['.svs', '.ndpi']:
+        raise Exception('invalid file format %r' % ex)
+    return ex[1:]
